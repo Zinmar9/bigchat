0f818ca5005db4e4397462445f35a1fac0542f0d
Zinmar9 <zinmarmyolwin2018@gmail.com>
2020-10-02 00:28:10 +0630

    Updates

---
+++ precache-manifest.83458c8b28bd0c2baafb65803d34203e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5910cb60ecc10fd1778872678f968e",
+    "url": "/chatmobileview/index.html"
+  },
+  {
+    "revision": "52c18eec73aa28c2e5ed",
+    "url": "/chatmobileview/static/css/main.e52c8f9c.chunk.css"
+  },
+  {
+    "revision": "48eef3460e980523ca1b",
+    "url": "/chatmobileview/static/js/2.f9d3e60c.chunk.js"
+  },
+  {
+    "revision": "97c0b5374351518bd0674c0fa3f02654",
+    "url": "/chatmobileview/static/js/2.f9d3e60c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52c18eec73aa28c2e5ed",
+    "url": "/chatmobileview/static/js/main.64ea5bdb.chunk.js"
+  },
+  {
+    "revision": "db90200c860dfaabcaf9",
+    "url": "/chatmobileview/static/js/runtime-main.ef8351ac.js"
+  },
+  {
+    "revision": "9299706f23346da2776f4aef1e52996a",
+    "url": "/chatmobileview/static/media/ic_wave_hand.9299706f.png"
+  },
+  {
+    "revision": "477fc8ddd40d200fe8f2635b5151a873",
+    "url": "/chatmobileview/static/media/mimi1.477fc8dd.gif"
+  },
+  {
+    "revision": "82a783353a27abe59feafd40bc686af1",
+    "url": "/chatmobileview/static/media/mimi2.82a78335.gif"
+  },
+  {
+    "revision": "477fc8ddd40d200fe8f2635b5151a873",
+    "url": "/chatmobileview/static/media/mimi3.477fc8dd.gif"
+  },
+  {
+    "revision": "6b65742393c65511ba223ec388c85ecc",
+    "url": "/chatmobileview/static/media/mimi4.6b657423.gif"
+  },
+  {
+    "revision": "609127cb5129f8e218fb57f059497c87",
+    "url": "/chatmobileview/static/media/mimi5.609127cb.gif"
+  },
+  {
+    "revision": "1ce35a6b3c21cc308a7683dfbea81c63",
+    "url": "/chatmobileview/static/media/mimi6.1ce35a6b.gif"
+  },
+  {
+    "revision": "ace546bd302229af3a0c7e7221d2dc10",
+    "url": "/chatmobileview/static/media/mimi7.ace546bd.gif"
+  },
+  {
+    "revision": "6646a3dec849498313a17c7de5d2ea5c",
+    "url": "/chatmobileview/static/media/mimi8.6646a3de.gif"
+  },
+  {
+    "revision": "b5ec7c32285b24c09eca4646a4511518",
+    "url": "/chatmobileview/static/media/mimi9.b5ec7c32.gif"
+  }
+]);
